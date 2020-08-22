6dabd57ab933ec28b0b47b050fb69956d7d348c4
Sven Sapia <svensapia@googlemail.com>
2020-08-22 12:03:12 +0200

    json owm api works

--- openweathermap_fetch.py
+++ openweathermap_fetch.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
 import json
+import urllib
 import urllib.request
 
 with open('./openweathermap_conf.json') as f:
   owmfile = json.load(f)
-print (owmfile['owm']['apikey'])
+
+owmWeather = (str(owmfile['owm']['url_weather'])) + (str(owmfile['owm']['str_loc'])) + (str(owmfile['owm']['location'])) + '&' + (str(owmfile['owm']['str_key'])) + (str(owmfile['owm']['apikey']))
+owmForecast = (str(owmfile['owm']['url_forecast'])) + (str(owmfile['owm']['str_loc'])) + (str(owmfile['owm']['location'])) + '&' + (str(owmfile['owm']['str_key'])) + (str(owmfile['owm']['apikey']))
+
+print (json.load(urllib.request.urlopen(owmWeather)))
+print (json.load(urllib.request.urlopen(owmForecast)))
